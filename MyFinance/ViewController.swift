1fa9f40f4f2727bad15dce466a2bfe69ab4a2442
kolesoking <kolesnikov.sergey.al@gmail.com>
2022-09-12 16:40:12 +0300

    Initial Commit

---
+++ MyFinance/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFinance
+//
+//  Created by Sergey Kolesnikov on 12.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
