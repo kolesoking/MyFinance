1e22492d8e7d878368a844039adfb1e41bad451b
kolesoking <kolesnikov.sergey.al@gmail.com>
2022-09-13 18:36:53 +0300

    DescriptionCell
    1. Add Description Cell
    2. Configure Description Cell

--- MyFinance/MainFlow/DitailModule/DitailViewController.swift
+++ MyFinance/MainFlow/DitailModule/DitailViewController.swift
@@ -37,6 +37,7 @@ private extension DitailViewController {
         ditailTableView.backgroundColor = Colors.mainBackgroundColor
         
         ditailTableView.register(UINib(nibName: "\(DitailTitleTableViewCell.self)", bundle: .main), forCellReuseIdentifier: "\(DitailTitleTableViewCell.self)")
+        ditailTableView.register(UINib(nibName: "\(DitailDescriptionTableViewCell.self)", bundle: .main), forCellReuseIdentifier: "\(DitailDescriptionTableViewCell.self)")
         ditailTableView.dataSource = self
         ditailTableView.delegate = self
     }
@@ -45,21 +46,31 @@ private extension DitailViewController {
 extension DitailViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 2
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = ditailTableView.dequeueReusableCell(withIdentifier: "\(DitailTitleTableViewCell.self)", for: indexPath)
-        
-        guard let cell = cell as? DitailTitleTableViewCell else {
+        switch indexPath.row {
+        case 0:
+            let cell = ditailTableView.dequeueReusableCell(withIdentifier: "\(DitailTitleTableViewCell.self)", for: indexPath)
+            guard let cell = cell as? DitailTitleTableViewCell else {
+                return UITableViewCell()
+            }
+            cell.money = "1200"
+            cell.currencyImageString = Icons.ruble
+            
+            return cell
+        case 1:
+            let cell = ditailTableView.dequeueReusableCell(withIdentifier: "\(DitailDescriptionTableViewCell.self)", for: indexPath)
+            guard let cell = cell as? DitailDescriptionTableViewCell else {
+                return UITableViewCell()
+            }
+            cell.mainDescription = "assdasfndjvnsjbvhsbfvkhjbriuybvhbkhjabvkhbdskvhbdkshbvkdhvbkhsbkdbhdbhbdfhjb"
+            
+            return cell
+        default:
             return UITableViewCell()
         }
-        
-        cell.money = "1200"
-        cell.currencyImageString = Icons.ruble
-        
-        
-        return cell
     }
     
     
