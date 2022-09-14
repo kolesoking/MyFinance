1feb04d95de9cf9a769a588501db3e131b0aa9b6
kolesoking <kolesnikov.sergey.al@gmail.com>
2022-09-14 22:39:00 +0300

    ConfigureAppearance NewOperationViewController
    1. Configure MainLanel
    2. Configure TextField
    3. Configure Button

--- MyFinance/MainFlow/NewOperationModule/NewOperationViewController.swift
+++ MyFinance/MainFlow/NewOperationModule/NewOperationViewController.swift
@@ -14,7 +14,7 @@ class NewOperationViewController: UIViewController {
     @IBOutlet weak var mainLabel: UILabel!
     @IBOutlet weak var operationSegmentedView: UISegmentedControl!
     @IBOutlet weak var moneyTextField: UITextField!
-    @IBOutlet weak var descriptionTextField: UITextField!
+    @IBOutlet weak var descriptionTextView: UITextView!
     @IBOutlet weak var saveOperationButton: UIButton!
     
     // MARK: - UIViewController
@@ -35,10 +35,39 @@ class NewOperationViewController: UIViewController {
 
 private extension NewOperationViewController {
     
+    // MARK: - ConfigureApearance
+    
     func configureAppearance() {
         
         view.backgroundColor = Colors.mainBackgroundColor
         
-        mainLabel.textColor = Colors.olderTextColor
+        mainLabel.textColor = Colors.mainTextColor
+        mainLabel.font = .systemFont(ofSize: 34, weight: .light)
+        mainLabel.text = "New Operation"
+        
+        operationSegmentedView.backgroundColor = Colors.cellBackgroundColor
+        operationSegmentedView.setTitle("Покупка", forSegmentAt: 0)
+        operationSegmentedView.setTitle("Стрижка", forSegmentAt: 1)
+        operationSegmentedView.selectedSegmentTintColor = Colors.mainBackgroundColor
+        let textAtributed = [NSAttributedString.Key.foregroundColor: Colors.mainTextColor]
+        operationSegmentedView.setTitleTextAttributes(textAtributed, for: .normal)
+        
+        moneyTextField.backgroundColor = Colors.cellBackgroundColor
+        moneyTextField.textColor = Colors.mainTextColor
+        moneyTextField.placeholder = "SUM"
+        
+        descriptionTextView.backgroundColor = Colors.cellBackgroundColor
+        descriptionTextView.textColor = Colors.mainTextColor
+        descriptionTextView.layer.cornerRadius = 5
+        
+        
+        saveOperationButton.backgroundColor = Colors.cellBackgroundColor
+        saveOperationButton.layer.cornerRadius = 5
+        saveOperationButton.setTitle("Cохранить", for: .normal)
+        saveOperationButton.setTitleColor(Colors.mainTextColor, for: .normal)
+        saveOperationButton.setTitleColor(Colors.olderTextColor, for: .highlighted)
+
+
+        
     }
 }
